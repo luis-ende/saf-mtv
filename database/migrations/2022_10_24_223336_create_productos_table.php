bdc647d6fd16421ecfdb08a342fe2cff593e5d17
Jose Luis Salazar <lsalazar@gmx.de>
2022-10-24 17:59:38 -0500

    Se crearon migraciones para tablas principales

---
+++ database/migrations/2022_10_24_223336_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('clave_cabms', 10);
+            $table->string('nombre');
+            $table->string('tipo');
+            $table->string('categoria')->nullable();
+            $table->string('subcategoria')->nullable();
+            $table->string('marca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
