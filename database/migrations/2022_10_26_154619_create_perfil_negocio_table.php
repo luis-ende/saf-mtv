ec6a25d0b28232e1f36f546d7685efcf3b0ae096
Jose Luis Salazar <lsalazar@gmx.de>
2022-10-26 17:48:23 -0500

    Se actualizaron migraciones

--- database/migrations/2022_10_26_154619_create_perfil_negocio_table.php
+++ database/migrations/2022_10_26_154619_create_perfil_negocio_table.php
@@ -16,14 +16,13 @@ return new class extends Migration
         Schema::create('perfil_negocio', function (Blueprint $table) {
             $table->id();   
             $table->foreignId('id_persona')->constrained('personas');
-            $table->text('lema_negocio');
-            $table->text('descripcion_negocio');
-            $table->string('diferenciadores');
-            $table->string('sitio_web', 255);
-            $table->string('cuenta_facebook', 240);
-            $table->string('cuenta_twitter', 240);
-            $table->string('cuenta_linkedin', 240);
-            $table->string('num_whatsapp', 15);
+            $table->text('lema_negocio')->nullable();
+            $table->text('descripcion_negocio')->nullable();            
+            $table->string('sitio_web', 255)->nullable();
+            $table->string('cuenta_facebook', 240)->nullable();
+            $table->string('cuenta_twitter', 240)->nullable();
+            $table->string('cuenta_linkedin', 240)->nullable();
+            $table->string('num_whatsapp', 15)->nullable();
             $table->timestamps();            
         });
     }
