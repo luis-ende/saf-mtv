ee49bc2f1e969ed8ae1aba4f5a300d0fb044249a
Jose Luis Salazar <lsalazar@gmx.de>
2022-10-26 21:39:58 -0500

    Se actualizaron migraciones

--- database/migrations/2013_10_11_000000_create_personas_table.php
+++ database/migrations/2013_10_11_000000_create_personas_table.php
@@ -22,16 +22,16 @@ return new class extends Migration
             $table->integer('id_asentamiento');
             $table->integer('id_tipo_vialidad');
             $table->string('vialidad', 120);
-            $table->string('num_int', 80);
-            $table->string('num_ext', 100);                        
+            $table->string('num_int', 80)->nullable();
+            $table->string('num_ext', 100);
             $table->string('lada', 8);
             $table->string('telefono_fijo', 10);
-            $table->string('extension', 8);
+            $table->string('extension', 8)->nullable();
             $table->string('telefono_movil', 12);
             $table->string('email', 255);
-            $table->string('email_alterno', 255);
-            $table->string('grupo_prioritario')->nullable();    
-            $table->timestamps();        
+            $table->string('email_alterno', 255)->nullable();
+            $table->string('grupo_prioritario')->nullable();
+            $table->timestamps();
         });
     }
 
