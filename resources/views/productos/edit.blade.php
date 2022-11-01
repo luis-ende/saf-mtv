d084dea4008bb755ef6d63d483f2ecaed1c6b685
Jose Luis Salazar <lsalazar@gmx.de>
2022-11-01 11:06:24 -0600

    Se agregó sección de fotos para la página de edición de productos

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -1,8 +1,34 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="container">
+        <ul class="nav nav-tabs mb-3" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a 
+                class="nav-link active" 
+                id="tab-detalles" 
+                data-bs-toggle="tab"
+                href="#tab-content-detalles" 
+                role="tab" 
+                aria-controls="tab-content-detalles"
+                aria-selected="true">Detalles del producto</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a 
+                class="nav-link" 
+                id="tab-fotos" 
+                data-bs-toggle="tab"
+                href="#tab-content-fotos" 
+                role="tab" 
+                aria-controls="tab-content-fotos"
+                aria-selected="false">Fotos</a>
+            </li>        
+        </ul>
+
+        <div class="tab-content" id="producto_tabs_content">
+            <div class="tab-pane fade show active" id="tab-content-detalles" role="tabpanel" aria-labelledby="tab-detalles">
                 <x-producto-form :mode="__('edit')" />
+            </div>
+            <div class="tab-pane fade" id="tab-content-fotos" role="tabpanel" aria-labelledby="tab-fotos">
+                <p>Fotos del producto</p>
             </div>
         </div>
     </div>
