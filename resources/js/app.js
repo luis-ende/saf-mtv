e2f28ad169718b0b0edc439bc49bdcae8c1a6f73
Jose Luis Salazar <lsalazar@gmx.de>
2022-11-01 22:28:01 -0600

    Se agregó soporte para bootstrap popovers

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
 
+import '../sass/app.scss';
+import * as bootstrap from 'bootstrap';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
