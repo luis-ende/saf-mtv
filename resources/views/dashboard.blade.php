9b6a5316f9461926fdb10e9ac7002999d4cd13b1
Jose Luis Salazar <lsalazar@gmx.de>
2022-11-15 11:19:00 -0600

    Ajuste de estilos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 min-h-fit">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg h-[42rem]">
                 <div class="p-6 bg-white border-b border-gray-200">
