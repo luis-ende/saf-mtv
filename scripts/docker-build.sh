306e2049f2c67b7d729daf9f440c280d55afded7
Jose Luis Salazar <lsalazar@gmx.de>
2022-11-22 14:47:04 -0600

    Cambios en script Docker

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -11,8 +11,9 @@ set -e # We want to fail at each command, to stop execution.
 cd ..
 docker compose build --build-arg user="$(whoami)" --build-arg uid="$(id -u)" app
 docker compose up -d
+docker compose exec app rm -f /var/www/public/storage
 docker compose exec app ln -s /var/www/storage/app/public /var/www/public/storage
-docker docker compose exec app composer install
-docker docker compose exec app npm install
-docker docker compose exec app npm run build
-docker docker compose exec app php artisan migrate
+docker compose exec app composer install
+docker compose exec app npm install
+docker compose exec app npm run build
+docker compose exec app php artisan migrate
