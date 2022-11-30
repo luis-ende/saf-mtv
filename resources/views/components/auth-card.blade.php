228a7bbab55cc5928082c13529779c46b84e4bd0
Jose Luis Salazar <lsalazar@gmx.de>
2022-11-30 17:54:05 -0600

    Ajuste de estilo

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0 h-[48rem]">
-    <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md px-6 py-4 bg-white overflow-hidden">
         {{ $slot }}
     </div>
 </div>
