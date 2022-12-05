53c359636e25d3ebd41a29f9e335a80cc6980c10
Jose Luis Salazar <lsalazar@gmx.de>
2022-12-05 11:37:32 -0600

    Ajuste en Dabaseseeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +14,140 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->seedTiposVialidad();
+    }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    private function seedTiposVialidad() 
+    {
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CALLE',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'AVENIDA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CALZADA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'EJE VIAL',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'ANDADOR',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CALLEJON',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'BOULEVARD',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CARRETERA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'PRIVADA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CIRCUNVALACIÓN',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'BRECHA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'DIAGONAL',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CORREDOR',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CIRCUITO',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'PASAJE',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'VEREDA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'VIADUCTO',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'PROLONGACIÓN',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'PEATONAL',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'RETORNO',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CAMINO',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CERRADA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'AMPLIACIÓN',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'CONTINUACIÓN',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'TERRACERÍA',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('cat_tipo_vialidad')->insert([
+            'tipo_vialidad' => 'PERIFÉRICO',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
