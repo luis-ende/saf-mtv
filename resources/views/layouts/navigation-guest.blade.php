91bc4c163322c36d7ccd256f2051719bdf03e7fd
Jose Luis Salazar <lsalazar@gmx.de>
2022-12-12 17:09:45 -0600

    Se removió paquete laravel-arcanist

--- resources/views/layouts/navigation-guest.blade.php
+++ resources/views/layouts/navigation-guest.blade.php
@@ -42,8 +42,20 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6 flex flex-row">
-                <a class="font-bold no-underline mr-5" href="{{ route('registro-inicio') }}">Regístrate</a>
-                <a class="font-bold no-underline mr-5" href="{{ route('login') }}">Ingresa</a>
+                @if (Auth::user())
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>                                                        
+                @else
+                    <a class="font-bold no-underline mr-5" href="{{ route('registro-inicio') }}">Regístrate</a>
+                    <a class="font-bold no-underline mr-5" href="{{ route('login') }}">Ingresa</a>
+                @endif
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -86,12 +98,24 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('login')">
-                {{ __('Ingresa') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('wizard.registro-mtv.create')">
-                {{ __('Regístrate') }}
-            </x-responsive-nav-link>
+            @if (Auth::user())
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                </form>                
+            @else
+                <x-responsive-nav-link :href="route('login')">
+                    {{ __('Ingresa') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('registro-inicio')">
+                    {{ __('Regístrate') }}
+                </x-responsive-nav-link>
+            @endif
             {{-- <x-responsive-nav-link :href="route('oportunidades-negocio')">
                 {{ __('Oportunidades') }}
             </x-responsive-nav-link>
