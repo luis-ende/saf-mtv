b5b9c3d3dca04f4b6c012b328f684a2fffe55441
Jose Luis Salazar <lsalazar@gmx.de>
2022-12-13 17:40:26 -0600

    Script para restaurar la base de datos local de desarrollo

---
+++ scripts/restore-local-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo "###############################################"
+echo "## Restaurando db local Homestead      ...   ##"
+echo "###############################################"
+echo ""
+
+php artisan migrate:fresh
+php artisan db:seed
+export PGPASSWORD='secret'
+psql -d saf_mtv -h homestead -U homestead -e -f /var/www/saf-mtv/Homestead/pgsql/cabms.sql
+psql -d saf_mtv -h homestead -U homestead -e -f /var/www/saf-mtv/Homestead/pgsql/cat_asentamientos.sql
