294567860279f4f087a027d7281deba726105b3a
Jose Luis Salazar <lsalazar@gmx.de>
2022-12-20 22:47:26 -0600

    Ajuste estilo perfil de negocio

--- resources/views/perfil-negocio.blade.php
+++ resources/views/perfil-negocio.blade.php
@@ -1,16 +1,16 @@
-<x-app-layout>    
+<x-app-layout>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden">
+        <div class="bg-white overflow-hidden min-h-screen">
             <div class="text-2xl p-6 bg-white border-b border-gray-200">
-                <div class="uppercase text-mtv-primary font-bold border-l-4 border-mtv-primary p-2"> 
+                <div class="uppercase text-mtv-primary font-bold border-l-4 border-mtv-primary p-2">
                     Mi perfil
-                </div>                    
-            </div>                
+                </div>
+            </div>
             <div class="px-4 py-4">
                 <div class="accordion" id="perfil-accordion">
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading-datos-contacto">
-                            <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#body-datos-contacto" aria-expanded="true" aria-controls="body-datos-contacto">                                    
+                            <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#body-datos-contacto" aria-expanded="true" aria-controls="body-datos-contacto">
                                 Tu negocio
                             </button>
                         </h2>
@@ -24,13 +24,13 @@
                                     :tipos_pyme="$tipos_pyme"
                                     :sectores="$sectores"
                                     :mode="__('edit')"
-                                />                                    
+                                />
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="heading-perfil-negocio">
-                            <button class="accordion-button fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#body-perfil-negocio" aria-expanded="false" aria-controls="collapseTwo">                                    
+                            <button class="accordion-button fw-bold collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#body-perfil-negocio" aria-expanded="false" aria-controls="collapseTwo">
                                 Contactos
                             </button>
                         </h2>
@@ -40,13 +40,13 @@
                                     @csrf
                                     <x-contactos-lista
                                         :persona="$persona"
-                                    />                                                
+                                    />
                                 </form>
                             </div>
                         </div>
                     </div>
-                </div>                
-            </div>    
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
