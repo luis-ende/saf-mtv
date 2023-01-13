fc77fe302397d9f22214db2d4f3a8e5aa1c9534e
Jose Luis Salazar <lsalazar@gmx.de>
2023-01-12 19:10:19 -0600

    Cambio de estilo en componente field-group-card

--- resources/views/components/field-group-card.blade.php
+++ resources/views/components/field-group-card.blade.php
@@ -1,6 +1,6 @@
 <div class="border rounded">
     <div class="border-b">
-        <label class="text-mtv-gold font-bold py-2 ml-5 text-lg">{{ $title }}</label>
+        <label class="text-mtv-primary font-bold py-2 ml-5 text-lg">{{ $title }}</label>
     </div>
     <div class="mx-3 pt-2 pb-3">
         {{ $slot }}
