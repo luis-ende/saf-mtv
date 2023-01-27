b06c669355901bd521ccfd50a137533a41c567e0
Jose Luis Salazar <lsalazar@gmx.de>
2023-01-27 11:27:21 -0600

    Ajuste botón compartir

--- resources/views/components/catalogo-productos/social-links.blade.php
+++ resources/views/components/catalogo-productos/social-links.blade.php
@@ -2,7 +2,7 @@
 
 <a href="{{ $links['whatsapp'] }}" target="popup" alt="Enlace compartir Whatsapp"   
     onclick="window.open('{{ $links['whatsapp'] }}','popup','width=600,height=600'); return false;"
-    class="{{ $button_style }} no-underline md:text-base xs:text-sm my-4">
-    @svg('icomoon-whatsapp', ['class' => 'w-5 h-5 inline-block mr-3 md:inline xs:hidden'])
+    class="{{ $button_style }} no-underline md:text-base xs:text-sm my-4 flex flex-row items-center">
+    @svg('icomoon-whatsapp', ['class' => 'w-5 h-5 mr-3 md:inline xs:hidden'])
     Compartir
 </a>
