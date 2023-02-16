dafc0dc3c3f379df15052995bb0a7462c9eff436
Jose Luis Salazar <lsalazar@gmx.de>
2023-02-16 14:39:35 -0600

    Ajustes a seeder de cat_etapas_procedimiento y layout responsivo

--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -10,22 +10,22 @@
                     tabActive: 'text-white bg-mtv-secondary hover:text-mtv-secondary', 
                     tabInactive: 'text-white bg-mtv-gold-light hover:text-white' }">
                     <nav class="font-bold text-lg flex md:flex-row xs:flex-col md:space-x-7 md:space-y-0 xs:space-y-4 xs:space-x-0 px-7 mx-auto mb-14 w-3/4">
-                        <button class="no-underline rounded-3xl basis-1/2 text-center py-2 px-5"
+                        <button class="no-underline rounded-3xl basis-1/2 text-center py-2"
                             :class="tab === 1 ? tabActive : tabInactive"
                             x-on:click.prevent="tab = 1">
                             {{ count($opn_sugeridas) }} Oportunidades sugeridas
                         </button>
-                        <button class="no-underline rounded-3xl basis-1/2 text-center py-2 px-5"
+                        <button class="no-underline rounded-3xl basis-1/2 text-center py-2"
                            :class="tab === 2 ?  tabActive : tabInactive"
                            x-on:click.prevent="tab = 2">
                            {{ count($opn_guardadas) }} Favoritos
                         </button>
                     </nav>
-                    <div x-show="tab === 1" class="px-5">
+                    <div x-show="tab === 1" class="px-2">
                         <x-oportunidades.oportunidades-grid
                             :oportunidades="$opn_sugeridas" />
                     </div>
-                    <div x-show="tab === 2" class="px-5">
+                    <div x-show="tab === 2" class="px-2">
                         <x-oportunidades.oportunidades-grid
                             :oportunidades="$opn_guardadas" />
                     </div>
