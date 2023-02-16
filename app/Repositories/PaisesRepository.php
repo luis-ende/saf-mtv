b44d076c5d6b1d233c5c359fa66c29dd7f8a508d
Jose Luis Salazar <lsalazar@gmx.de>
2023-02-16 14:38:55 -0600

    Implementación de caché para tabla cat_paises

--- app/Repositories/PaisesRepository.php
+++ app/Repositories/PaisesRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class PaisesRepository
 {
@@ -10,7 +11,9 @@ class PaisesRepository
      * @return \Illuminate\Support\Collection
      */
     public static function obtienePaises() {
-        return DB::table('cat_paises')->get();
+        return Cache::rememberForever('cat_paises', function() {
+            return DB::table('cat_paises')->get();
+        });        
     }
 
     public static function obtienePaisNombre(int $paisId): ?string
