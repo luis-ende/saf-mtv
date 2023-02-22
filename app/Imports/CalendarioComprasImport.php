95ecf88f3899b53a292c87a4496df5644fa6759e
Jose Luis Salazar <lsalazar@gmx.de>
2023-02-22 12:26:46 -0600

    Actualización importador de calendario de compras

--- app/Imports/CalendarioComprasImport.php
+++ app/Imports/CalendarioComprasImport.php
@@ -5,9 +5,18 @@ namespace App\Imports;
 use App\Models\CalendarioCompra;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use App\Repositories\OportunidadNegocioRepository;
+use Illuminate\Support\Collection;
 
 class CalendarioComprasImport implements ToModel, WithHeadingRow
 {
+    private Collection $unidadesCompradoras;
+
+    public function __construct(OportunidadNegocioRepository $oportunidadRepo)
+    {
+        $this->unidadesCompradoras = $oportunidadRepo->obtieneInstitucionesCompradoras();
+    }
+
     /**
     * @param array $row
     *
@@ -15,6 +24,7 @@ class CalendarioComprasImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {    
+        
         return new CalendarioCompra([
             //
         ]);
