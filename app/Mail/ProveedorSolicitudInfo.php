634d675f8c1aed0baf9eab83a2736d05d0e224cd
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-07 11:34:57 -0600

    Implementación de servicio de envio de correo para mensajes de solicitud de información de URG a proveedor

---
+++ app/Mail/ProveedorSolicitudInfo.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\UsuarioURG;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ProveedorSolicitudInfo extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        public UsuarioURG $usuarioURG,
+        public string $asunto,
+        public string $mensaje
+    ) { }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            from: new Address($this->usuarioURG->email, $this->usuarioURG->nombre),
+            subject: $this->asunto,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.proveedor-solicitud-info',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
