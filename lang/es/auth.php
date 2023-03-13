7dfded52a9243a6ca0c50ac7d8a095533a170b85
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-13 16:32:28 -0600

    Se agregó paquete de traducciones leyendas al español

--- lang/es/auth.php
+++ lang/es/auth.php
@@ -1,20 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autenticación de líneas de lenguaje
-    |--------------------------------------------------------------------------
-    |
-    | Las siguientes líneas de idioma se utilizan durante la autenticación para varios
-    | Mensajes que necesitamos mostrar al usuario. Eres libre de modificar
-    | Estas líneas lingüísticas de acuerdo a los requisitos de su aplicación.
-    |
-    */
-
-    'failed' => 'Las credenciales introducidas son incorrectas.',
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña es incorrecta.',
-    'throttle' => 'Demasiados intentos de acceso. Inténtelo de nuevo en :seconds segundos.',
-
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
 ];
--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -1,22 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Restablecer contraseña de líneas de idioma
-    |--------------------------------------------------------------------------
-    |
-    | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden con las razones
-    | que proporciona la contraseñas de los usuario para un intento de actualización que
-    | es invalida, por ejemplo, para un token no válido o una nueva contraseña no válida.
-    |
-    */
-
-    'password' => 'Las contraseñas deben contener al menos 6 caracters y coincidir.',
-    'reset' => 'Su contraseña ha sido cambiada.',
-    'sent' => 'Le hemos enviado un correo de cambio de contraseña!',
-    'token' => 'El código de cambio de contraseña es inválido.',
-    'user' => "No podemos encontrar un usuario con ese nombre.",
-
+    'password'  => 'Las contraseñas deben contener al menos 6 caracters y coincidir.',
+    'reset'     => 'Su contraseña ha sido restablecida.',
+    'sent'      => 'Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.',
+    'throttled' => 'Por favor espere antes de intentar de nuevo.',
+    'token'     => 'El token de restablecimiento de contraseña es inválido.',
+    'user'      => 'No encontramos ningún usuario con ese correo electrónico.',
 ];
