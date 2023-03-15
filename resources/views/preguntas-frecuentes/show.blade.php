5110838b935ce06bbf7c176a866a9a839a122181
Lili <ravalidesign@gmail.com>
2023-03-15 10:17:54 -0600

    Se agregaron archivos de estilos para botones en hover y active

--- resources/views/preguntas-frecuentes/show.blade.php
+++ resources/views/preguntas-frecuentes/show.blade.php
@@ -32,26 +32,27 @@
 
     <div class="tab-wrapper" x-data="{ activeTab:  -1 }">
         <div class="w-full flex justify-center border-b border-gray-200 pb-6 mb-20">
-            <div class="flex flex-row items-center space-x-40 mt-2  w-1/2 buttons-content">
-                <label @click="activeTab = 0"
-                    class="tab-control group hover:text-base hover:text-mtv-secondary  hover:font-bold"
-                    :class="{ 'active': activeTab === 0 }">
-                    @svg('general-public',
-                    ['class' => 'h-16 w-16 mb-px mt-3 ml-7 group-hover:scale-150 group-hover:mb-6'])
-                    Público en general
-                </label>
-                <label @click="activeTab = 1"
-                    class="tab-control group hover:text-base hover:text-mtv-secondary  hover:font-bold"
-                    :class="{ 'active': activeTab === 1 }">
-                    @svg('register',
-                    ['class' => 'h-20 w-20 group-hover:scale-150 group-hover:mb-6'])
-                    Proveedores</label>
-                <label @click="activeTab = 2"
-                    class="tab-control group hover:text-base hover:text-mtv-secondary  hover:font-bold"
-                    :class="{ 'active': activeTab === 2 }">
-                    @svg('institutions',
-                    ['class' => 'h-20 w-20 ml-9 group-hover:scale-150 group-hover:mb-6 group-hover:ml-18'])
-                    Instituciones compradoras</label </label>
+            <div class="flex flex-row items-center space-x-40 mt-2  w-1/2 ">
+                <div :class="activeTab === 0 ? 'buttons-content-active' : 'buttons-content'">
+                    <label @click="activeTab = 0" class="tab-control " :class="{ 'active': activeTab === 0 }">
+                        @svg('general-public',
+                        ['class' => 'h-16 w-16 mb-px mt-3 ml-7 '])
+                        <p class="text-button">Público en general</p>
+                    </label>
+                </div>
+                <div :class="activeTab === 1 ? 'buttons-content-active' : 'buttons-content'">
+                    <label @click="activeTab = 1" class="tab-control" :class="{ 'active': activeTab === 1 }">
+                        @svg('register',
+                        ['class' => 'h-20 w-20'])
+                        <p class="text-button-pd">Proveedores </p>
+                    </label>
+                </div>
+                <div :class="activeTab === 2 ? 'buttons-content-active' : 'buttons-content'">
+                    <label @click="activeTab = 2" class="tab-control " :class="{ 'active': activeTab === 2 }">
+                        @svg('institutions',
+                        ['class' => 'h-20 w-20 ml-9'])
+                        Instituciones compradoras</label>
+                </div>
             </div>
         </div>
 
@@ -171,18 +172,6 @@
         </div>
     </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
     <!-- Formulario  -->
     <div class="py-6 md:px-12 xs:px-6 bg-white  flex flex-col">
         <div class="self-center">
@@ -210,50 +199,53 @@
             </span>
         </div>
     </div>
-    <div class="flex flex-col mb-20">
-        <div class="flex justify-center w-3/5 ml-80">
-            <div class="mtv-input-wrapper mr-3 basis-1/3">
-                <input type="text" class="mtv-text-input" id="nombre" name="nombre">
-                <label class="mtv-input-label" for="nombre">Nombre</label>
-            </div>
-            <div class="mtv-input-wrapper mr-3 basis-1/3">
-                <input type="text" class="mtv-text-input" id="alcaldia" name="alacaldia">
-                <label class="mtv-input-label" for="nombre">Alcaldía o Ciudad</label>
-            </div>
-            <div class="mtv-input-wrapper basis-1/3">
-                <input type="text" class="mtv-text-input" id="mail" name="mail">
-                <label class="mtv-input-label" for="nombre">Correo electrónico</label>
-            </div>
-        </div>
-        <div class="flex justify-center ml-80 w-3/5">
-            <div class="flex flex-col w-1/5">
-                <div class="flex mt-3 mb-3">
-                    <input type="radio" class="self-center mr-4 focus:ring-slate-200" id="tipo_persona_fisica"
-                        name="tipo_persona">
-                    <label class="mr-3 text-l text-mtv-gray" for="tipo_persona_fisica">Física</label>
-                    <input type="radio" class="self-center mr-4 focus:ring-slate-200" id="tipo_persona_moral"
-                        name="tipo_persona">
-                    <label class="text-mtv-gray" for="tipo_persona_moral">Moral
-                    </label>
+    <form method="POST" action="">
+        @csrf
+        <div class="flex flex-col mb-20">
+            <div class="flex justify-center w-3/5 ml-80">
+                <div class="mtv-input-wrapper mr-3 basis-1/3">
+                    <input type="text" class="mtv-text-input" id="nombre" name="nombre">
+                    <label class="mtv-input-label" for="nombre">Nombre</label>
                 </div>
-                <div>
-                    <div class="mtv-input-wrapper">
-                        <select class="mtv-text-input" id="tipo_de_empresa" name="empresa">
-                            <option value="0">Tipo de empresa</option>
-                        </select>
+                <div class="mtv-input-wrapper mr-3 basis-1/3">
+                    <input type="text" class="mtv-text-input" id="alcaldia" name="alacaldia">
+                    <label class="mtv-input-label" for="nombre">Alcaldía o Ciudad</label>
+                </div>
+                <div class="mtv-input-wrapper basis-1/3">
+                    <input type="text" class="mtv-text-input" id="mail" name="mail">
+                    <label class="mtv-input-label" for="nombre">Correo electrónico</label>
+                </div>
+            </div>
+            <div class="flex justify-center ml-80 w-3/5">
+                <div class="flex flex-col w-1/5">
+                    <div class="flex mt-3 mb-3">
+                        <input type="radio" class="self-center mr-4 focus:ring-slate-200" id="tipo_persona_fisica"
+                            name="tipo_persona">
+                        <label class="mr-3 text-l text-mtv-gray" for="tipo_persona_fisica">Física</label>
+                        <input type="radio" class="self-center mr-4 focus:ring-slate-200" id="tipo_persona_moral"
+                            name="tipo_persona">
+                        <label class="text-mtv-gray" for="tipo_persona_moral">Moral
+                        </label>
+                    </div>
+                    <div>
+                        <div class="mtv-input-wrapper">
+                            <select class="mtv-text-input" id="tipo_de_empresa" name="empresa">
+                                <option value="0">Tipo de empresa</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-4/5">
+                    <div class="mtv-input-wrapper ml-3">
+                        <textarea class="mtv-text-input" id="mensaje" name="mensaje" rows="3" cols="50"></textarea>
+                        <label class="mtv-input-label" for="mensaje">Mensaje</label>
                     </div>
                 </div>
             </div>
-            <div class="w-4/5">
-                <div class="mtv-input-wrapper ml-3">
-                    <textarea class="mtv-text-input" id="mensaje" name="mensaje" rows="3" cols="50"></textarea>
-                    <label class="mtv-input-label" for="mensaje">Mensaje</label>
-                </div>
+            <div :class="{'fixed bottom-0 left-0 bg-white w-full border-t-2': filtrosModalOpen}"
+                class="flex flex-row space-x-3 items-center justify-center">
+                <button type="submit" class="mtv-button-secondary">Enviar mensaje</button>
             </div>
         </div>
-        <div :class="{'fixed bottom-0 left-0 bg-white w-full border-t-2': filtrosModalOpen}"
-            class="flex flex-row space-x-3 items-center justify-center">
-            <button type="submit" class="mtv-button-secondary">Enviar mensaje</button>
-        </div>
-    </div>
+    </form>
 </x-guest-layout>
