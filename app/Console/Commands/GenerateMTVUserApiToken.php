c1a909651f1464654e39aaeb7d5e0a42cec5b479
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-17 12:23:55 -0600

    Se implemento comando artisan para generar tokens de autenticación en servidor

---
+++ app/Console/Commands/GenerateMTVUserApiToken.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class GenerateMTVUserApiToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mtv:gen-token {user_id} {token_name=authToken}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Genera API Tokens para usuarios de MTV.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): string
+    {
+        $user = User::find($this->argument('user_id'));
+        if ($user) {
+            $token = $user->createToken($this->argument('token_name'));
+            $this->info("Token generado para el usuario {$user->rfc}: {$token->plainTextToken}");
+        }
+
+        return Command::SUCCESS;
+    }
+}
