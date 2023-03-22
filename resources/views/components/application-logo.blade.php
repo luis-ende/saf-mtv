6c633e2a3052b486b9bac4432b21e0e74b59e680
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-22 12:18:15 -0600

    Se agregaron svg de logos para el encabezado

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand" href="{{ route('homepage') }}" style="display: flex">
-    <img src="https://tianguisdigital.finanzas.cdmx.gob.mx/images/logos/gobierno_cdmx_con.svg" class="LogoCDMX w-36 h-auto" alt="Logo CDMX">
-    <img src="https://tianguisdigital.finanzas.cdmx.gob.mx/images/logos/tianguis_digital.svg" class="LogoTianguis w-16 h-auto ml-3" alt="Logo Tianguis Digital">
+    <img src="{{ asset('images/logos/gobierno_cdmx_con.svg') }}" class="LogoCDMX w-36 h-auto" alt="Logo CDMX">
+    <img src="{{ asset('images/logos/tianguis_digital.svg') }}" class="LogoTianguis w-16 h-auto ml-3" alt="Logo Tianguis Digital">
 </a>
