39017d50e451af3bef0d992a5f6da8023af0ea82
Jose Luis Salazar <lsalazar@gmx.de>
2023-03-27 11:00:06 -0600

    Cambio en migración para registro de proveedores por API endpoint

--- database/migrations/2013_10_11_000000_create_personas_table.php
+++ database/migrations/2013_10_11_000000_create_personas_table.php
@@ -27,6 +27,8 @@ return new class extends Migration
             $table->string('num_int', 80)->nullable();
             $table->string('email');
             $table->unsignedSmallInteger('registro_fase')->default(1);
+            // 1 = Proveedor registrado en MTV | 2 = Proveedor importado de Padrón de Roveedores
+            $table->unsignedSmallInteger('id_tipo_registro')->default(1);
             $table->timestamps();
         });
     }
