2a6c1bcc85ffbca1b4f7cc9dec17e237c2c9ba6e
Jose Luis Salazar <lsalazar@gmx.de>
2023-04-10 14:52:24 -0600

    Se actualizó seeder de preguntas frecuentes para importar registros con estilos aplicados (negritas, enlaces,etc.)

--- database/seeders/PreguntasFrecuentesSeeder.php
+++ database/seeders/PreguntasFrecuentesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Imports\PreguntasFrecuentesImport;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 
 class PreguntasFrecuentesSeeder extends Seeder
@@ -15,11 +16,20 @@ class PreguntasFrecuentesSeeder extends Seeder
      */
     public function run()
     {
+        $path = base_path('database/data/preguntas_frecuentes.sql');
+        $sql = file_get_contents($path);
+
+        DB::unprepared($sql);
+    }
+
+    public function importFromExcel()
+    {
+        // Importación original de datos
         $path = base_path('database/data/PREGUNTAS FRECUENTES MTV.xlsx');
         $import = new PreguntasFrecuentesImport();
         $import->onlySheets('Público > Conceptos', 'Público > Compras públicas', 'Público > MTV',
-                            'Proveedores > Padrón de Proveed', 'Proveedores > Precotizaciones',
-                            'Instituciones compradoras');
+            'Proveedores > Padrón de Proveed', 'Proveedores > Precotizaciones',
+            'Instituciones compradoras');
 
         Excel::import($import, $path);
     }
