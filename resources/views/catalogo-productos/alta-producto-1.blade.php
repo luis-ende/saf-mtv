659300a42ef861059d7d914ca9fc12fb1419af2b
Jose Luis Salazar <lsalazar@gmx.de>
2023-04-11 10:54:35 -0600

    Ajustes de estilo en pantallas de registro de producto

--- resources/views/catalogo-productos/alta-producto-1.blade.php
+++ resources/views/catalogo-productos/alta-producto-1.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout :show_main_menu="false">
     @section('page_title', 'Alta de producto 1')
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 my-4">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4">
         <div class="bg-white overflow-hidden shadow-sm h-screen">
             @include('catalogo-productos.registro-header',
                        ['titulo' => 'Agrega tu producto',
