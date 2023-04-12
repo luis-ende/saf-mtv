f4b53f1dcb944b93db44a7464195f43d8934aff1
Jose Luis Salazar <lsalazar@gmx.de>
2023-04-12 10:44:23 -0600

    Cambio de logo principal de encabezado

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <a class="navbar-brand" href="{{ route('homepage') }}" style="display: flex">
-    <img src="{{ asset('images/logos/gobierno_cdmx_con.svg') }}" class="LogoCDMX w-36 h-auto" alt="Logo CDMX">
-    <img src="{{ asset('images/logos/tianguis_digital.svg') }}" class="LogoTianguis w-16 h-auto ml-3" alt="Logo Tianguis Digital">
+    <img src="{{ asset('images/logos/LOGO_SAF_2021-01.svg') }}" class="LogoCDMX w-64 h-auto" alt="Logo CDMX">
+    <img src="{{ asset('images/logos/tianguis_digital.svg') }}" class="LogoTianguis w-16 h-auto" alt="Logo Tianguis Digital">
 </a>
