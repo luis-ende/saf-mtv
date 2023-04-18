be1adad5649399a690f60a9425dd300f39f1dc2d
Jose Luis Salazar <lsalazar@gmx.de>
2023-04-18 15:57:40 -0600

    Ajuste en carrusel de escritorio para agregar animación de slides

--- resources/views/components/escritorio-proveedor/carrusel-seccion.blade.php
+++ resources/views/components/escritorio-proveedor/carrusel-seccion.blade.php
@@ -4,8 +4,8 @@
      x-init="initCarrusel()"
      x-cloak
      class="h-full">
-    <template x-for="slide in slides" :key="slide.id">
-        <div x-show="activeSlide === slide.id"
+    <template x-for="(slide, index) in slides" :key="index">
+        <div x-show="activeSlide === index"
              class="h-full flex flex-row bg-transparent">
             <a :href="slide.enlace" target="_blank"
                class="md:w-auto w-full h-[95%]">
@@ -15,14 +15,14 @@
         </div>
     </template>
     <div class="h-1 w-1/3 flex items-center justify-center mx-auto">
-        <template x-for="slide in slides" :key="slide.id">
+        <template x-for="(slide, index) in slides" :key="index">
             <button
                     class="flex-1 w-1 h-2 mt-4 mx-2 mb-0 rounded-full overflow-hidden transition-colors duration-200 ease-out hover:bg-mtv-secondary hover:shadow-lg"
                     :class="{
-                              'bg-mtv-gold-light': activeSlide === slide.id,
-                              'bg-mtv-gray-light': activeSlide !== slide.id
+                              'bg-mtv-gold-light': activeSlide === index,
+                              'bg-mtv-gray-light': activeSlide !== index
                             }"
-                    @click="activeSlide = slide.id"
+                    @click="activeSlide = index"
             ></button>
         </template>
     </div>
@@ -32,11 +32,19 @@
     <script type="text/javascript">
         function carruselSeccion() {
             return {
-                activeSlide: 1,
+                activeSlide: 0,
                 slides: @js($slides),
                 initCarrusel() {
                     if (this.slides.length > 0) {
-                        this.activeSlide = this.slides[0].id;
+                        this.activeSlide = 0;
+
+                        setInterval(() => {
+                            if (this.activeSlide >= (this.slides.length - 1)) {
+                                this.activeSlide = 0;
+                            } else {
+                                this.activeSlide += 1;
+                            }
+                        }, 5000);
                     }
                 }
             }
